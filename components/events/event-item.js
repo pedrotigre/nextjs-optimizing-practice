f5afab4fad40df0e57bf5b97328ce99f2cc5c06c
Pedro Tigre <pedrogtigre@gmail.com>
2022-07-30 18:52:59 -0300

    next.js Image added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -3,6 +3,7 @@ import DateIcon from '../icons/date-icon';
 import AddressIcon from '../icons/address-icon';
 import ArrowRightIcon from '../icons/arrow-right-icon';
 import classes from './event-item.module.css';
+import Image from 'next/image';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -17,7 +18,14 @@ function EventItem(props) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <Image
+        src={'/' + image}
+        alt={title}
+        width={250}
+        height={160}
+        priority={true}
+      />
+      {/* priority preloads the image and remove lazyload, its good for largest contentful paint */}
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
